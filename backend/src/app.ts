6fa604dc3fb801f6757927d9564ffed690c35103
vishaljati <jativishal120@gmail.com>
2026-01-04 00:49:53 +0530

    feat: debug cors option

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,7 @@ app.use(
     credentials: true,
   })
 );
-app.options("*", cors());
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
