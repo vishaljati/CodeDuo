dae4535fac810bbadb430e8d173de1ac0780f1ee
vishaljati <jativishal120@gmail.com>
2026-01-03 23:48:32 +0530

    Add cors resource

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,16 @@
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
 
 const app: Application = express();
+app.use(
+  cors({
+    origin: process.env.REACT_CLIENT_URL || "http://localhost:5173",
+  })
+);
 
-app.use(express.json());
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
