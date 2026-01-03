cc7a0661aa54c22bf4fcf7ffbc2ec4c639bbd870
vishaljati <jativishal120@gmail.com>
2026-01-04 00:03:57 +0530

    Remove cors and add proxy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,8 @@
 import express, { Application, Request, Response } from "express";
-import cors from "cors";
+
 
 const app: Application = express();
-app.use(
-  cors({
-    origin: process.env.REACT_CLIENT_URL || "http://localhost:5173",
-  })
-);
+
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
