f3e5c3c52a8678a5e45e70e84f15cc5836e573ad
vishaljati <jativishal120@gmail.com>
2026-01-03 22:05:10 +0530

    Add api url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      "/api":"http://localhost:3000"
+      "/api":"https://api-codeduo.vercel.app"
     },
   },
 })
