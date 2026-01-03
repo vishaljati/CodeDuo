5ebdea3a08f41b8f718e8340985a91fb5c02faec
vishaljati <jativishal120@gmail.com>
2026-01-03 11:26:50 +0530

    Add clerk auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,24 @@
-
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton
+} from '@clerk/clerk-react';
 
 function App() {
 
   return (
     <>
-      <h1>Hello World!</h1>
+      <header>
+        <SignedOut>
+          <SignInButton mode='modal'/>
+        </SignedOut>
+
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+
+      </header>
     </>
   )
 }
