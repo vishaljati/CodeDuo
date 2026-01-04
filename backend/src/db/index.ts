df5ad9465d109502a1fab9148d6770b38b8420de
vishaljati <jativishal120@gmail.com>
2026-01-04 18:38:06 +0530

    feat: debug mongodb connection

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -5,7 +5,7 @@ import { DB_Name } from '../constant.js';
 const connectDB = async () => {
   try {
     const connectionInstant = await mongoose.connect(
-      `${process.env.MONGODB_URL}/${ DB_Name }`
+      `${process.env.MONGODB_URL}/${DB_Name}`
     );
 
     console.log(
@@ -14,8 +14,8 @@ const connectDB = async () => {
     );
   } catch (error) {
     if (error instanceof Error) {
-        console.log('MONGODB CONNECTION FAILED :', error.message);
-        process.exit(1);
+      console.log('MONGODB CONNECTION FAILED :', error.message);
+      process.exit(1);
     }
     console.log('MONGODB CONNECTION FAILED :', error);
     process.exit(1);
